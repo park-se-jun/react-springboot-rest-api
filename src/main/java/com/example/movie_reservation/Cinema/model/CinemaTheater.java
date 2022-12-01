9816fe7f78d3b4ec5131791c59e7b7d91f0d833f
박세준 <tpwns6693@gmail.com>
2022-12-01 16:51:47 +0900

    refactor : CinemaTheater id 필드 이름 변경

--- src/main/java/com/example/movie_reservation/Cinema/model/CinemaTheater.java
+++ src/main/java/com/example/movie_reservation/Cinema/model/CinemaTheater.java
@@ -3,12 +3,12 @@ package com.example.movie_reservation.Cinema.model;
 import java.util.UUID;
 
 public class CinemaTheater {
-    private final UUID id;
+    private final UUID cinemaTheaterId;
     private final UUID cinemaId;
     private final UUID theaterId;
 
-    public CinemaTheater(UUID id, UUID cinemaId, UUID theaterId) {
-        this.id = id;
+    public CinemaTheater(UUID cinemaTheaterId, UUID cinemaId, UUID theaterId) {
+        this.cinemaTheaterId = cinemaTheaterId;
         this.cinemaId = cinemaId;
         this.theaterId = theaterId;
     }
