920c6a85efea8db4ef278e4b41049586323c2d59
박세준 <tpwns6693@gmail.com>
2022-12-01 16:51:26 +0900

    refactor : user 클래스 이름 변경 - User -> Customer

--- src/main/java/com/example/movie_reservation/customer/Customer.java
+++ src/main/java/com/example/movie_reservation/customer/Customer.java
@@ -1,17 +1,17 @@
-package com.example.movie_reservation.user;
+package com.example.movie_reservation.customer;
 
 import org.springframework.lang.NonNull;
 
 import java.util.UUID;
 
 //엔티티
-public class User {
+public class Customer {
     @NonNull
     final UUID id;
     @NonNull
     final String  phone;
 
-    public User(@NonNull UUID id, @NonNull String phone) {
+    public Customer(@NonNull UUID id, @NonNull String phone) {
         this.id = id;
         this.phone = phone;
     }
