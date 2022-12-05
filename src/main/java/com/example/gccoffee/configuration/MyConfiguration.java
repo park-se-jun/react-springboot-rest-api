8583e46b4b63fa9a9a1823dd6aabb983488ce0c8
박세준 <tpwns6693@gmail.com>
2022-12-06 02:23:38 +0900

    feat : health 체크 추가

--- src/main/java/com/example/gccoffee/configuration/MyConfiguration.java
+++ src/main/java/com/example/gccoffee/configuration/MyConfiguration.java
@@ -9,5 +9,6 @@ public class MyConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**").allowedOrigins("*");
+        registry.addMapping("/health").allowedOrigins("*");
     }
 }
