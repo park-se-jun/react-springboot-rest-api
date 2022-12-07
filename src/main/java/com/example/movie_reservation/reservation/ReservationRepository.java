2bbcb6523545ca98ecc87f2ecc83c3153b05b9e7
박세준 <tpwns6693@gmail.com>
2022-12-07 19:05:02 +0900

    fix : createMovieReservation메소드 db접근이 제대로 실행되지 않는것 수정 - paramMap에 row와 col을 넣을 때 get(i)로 넣어줘야 해당 값이 들어가는데 indexOf를 써서 계속 -1로 들어갔었음 - reservationSeat에 넣을때 넣는데 실패하면 error throw 함

--- src/main/java/com/example/movie_reservation/reservation/ReservationRepository.java
+++ src/main/java/com/example/movie_reservation/reservation/ReservationRepository.java
@@ -1,6 +1,5 @@
 package com.example.movie_reservation.reservation;
 
-import com.example.gccoffee.repository.JdbcUtils;
 import com.example.movie_reservation.reservation.model.MovieReservation;
 import com.example.movie_reservation.reservation.model.ReservedSeat;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -11,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.*;
 
-import static com.example.gccoffee.repository.JdbcUtils.*;
+import static com.example.gccoffee.repository.JdbcUtils.toUUID;
 
 @Repository
 public class ReservationRepository {
@@ -36,34 +35,40 @@ public class ReservationRepository {
             List<Integer> colArray
     ) {
         Map<String, Object> paramMap = new HashMap<>();
-        paramMap.put("movieTicketId", movieTicketId);
+        paramMap.put("movieTicketId", movieTicketId.toString());
         paramMap.put("price", price);
         paramMap.put("reservationTime", reservationTime);
         paramMap.put("seatCount", seatCount);
         paramMap.put("phoneNumber", phoneNumber);
-        paramMap.put("scheduleId", scheduleId);
-        jdbcTemplate.update(
+        paramMap.put("scheduleId", scheduleId.toString());
+        var updatedFirst = jdbcTemplate.update(
                 "insert into movie_ticket(movie_ticket_id,price,user_phone_number,seat_count,reservation_time) " +
                         "values(UUID_TO_BIN(:movieTicketId),:price,:phoneNumber,:seatCount,:reservationTime)",
                 paramMap);
         // 제대로 넣어졌다면? -> 예약석도 넣어야함
-        for (int i = 0; i < rowArray.size(); i++) {
-            paramMap.put("row", rowArray.indexOf(i));
-            paramMap.put("col", colArray.indexOf(i));
-            jdbcTemplate.update(
-                    "insert into reserved_seat(" +
-                            "movie_ticket_movie_ticket_id," +
-                            "movie_schedule_movie_schedule_id," +
-                            "seat_seat_id)" +
-                            " select " +
-                            "UUID_TO_BIN(:movieRicketId)," +
-                            "UUID_TO_BIN(:scheduleId), " +
-                            "UUID_TO_BIN(s.seat_id)" +
-                            " from seat s join movie_schedule ms on " +
-                            " s.screen_screen_id = ms.screen_screen_id " +
-                            " where s.seat_row =:row and s.seat_col=:col",
-                    paramMap
-            );
+        if (updatedFirst == 1) {
+
+            for (int i = 0; i < rowArray.size(); i++) {
+                paramMap.put("row", rowArray.get(i));
+                paramMap.put("col", colArray.get(i));
+                var update = jdbcTemplate.update(
+                        """
+                                insert into reserved_seat(movie_ticket_movie_ticket_id,movie_schedule_movie_schedule_id,seat_seat_id) 
+                                select UUID_TO_BIN(:movieTicketId),
+                                       UUID_TO_BIN(:scheduleId),
+                                       s.seat_id
+                                from seat s join movie_schedule ms on
+                                            ms.movie_schedule_id=UUID_TO_BIN(:scheduleId) and
+                                            s.screen_screen_id = ms.screen_screen_id
+                                where s.seat_row = :row
+                                  and s.seat_col = :col;""",
+                        paramMap
+                );
+                System.out.println(update);
+                if (update != 1) {
+                    throw new RuntimeException("삽입 실패");
+                }
+            }
         }
 
         return movieTicketId;
@@ -73,7 +78,7 @@ public class ReservationRepository {
 
         return jdbcTemplate.query(
                 "select  * from movie_ticket where user_phone_number = :phoneNumber",
-                Collections.singletonMap("phoneNumber",phoneNumber),
+                Collections.singletonMap("phoneNumber", phoneNumber),
                 (rs, rowNum) -> {
                     var ticketId = toUUID(rs.getBytes("movie_ticket_id"));
                     var userPhoneNumber = rs.getString("user_phone_number");
@@ -81,22 +86,22 @@ public class ReservationRepository {
                     var price = rs.getLong("price");
                     var reservationTime = rs.getTimestamp("reservation_time").toLocalDateTime();
 
-                    return new MovieReservation(ticketId,phoneNumber,seatCount,price,reservationTime);
+                    return new MovieReservation(ticketId, phoneNumber, seatCount, price, reservationTime);
                 }
         );
     }
 
-    public List<ReservedSeat> getReservedSeatByMovieReservationId(UUID reservationId){
+    public List<ReservedSeat> getReservedSeatByMovieReservationId(UUID reservationId) {
         return jdbcTemplate.query(
                 "select s.seat_col,s.seat_row reserved_seat_id,seat_seat_id,movie_schedule_movie_schedule_id from reserved_seat join seat s on s.seat_id = reserved_seat.seat_seat_id where movie_ticket_movie_ticket_id=:reservationId",
-                Collections.singletonMap("reservationId",reservationId),
+                Collections.singletonMap("reservationId", reservationId),
                 (rs, rowNum) -> {
                     var reservedSeatId = toUUID(rs.getBytes("reserved_seat_id"));
                     var seatRow = rs.getString("seat_row");
                     var seatCol = rs.getInt("seat_col");
                     var scheduleId = toUUID(rs.getBytes("movie_schedule_movie_schedule_id"));
                     var seatId = toUUID(rs.getBytes("seat_id"));
-                    return new ReservedSeat(reservedSeatId,seatId,scheduleId,seatRow,seatCol);
+                    return new ReservedSeat(reservedSeatId, seatId, scheduleId, seatRow, seatCol);
                 });
     }
 }
